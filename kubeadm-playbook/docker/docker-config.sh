70992a34f862d57dafcec074987a038e9e266c5e
NadeemRais <nadeemmalik9800@gmail.com>
2023-05-22 16:17:02 +0530

    ansible-playbook added

---
+++ kubeadm-playbook/docker/docker-config.sh
@@ -0,0 +1,9 @@
+cat <<EOF | sudo tee /etc/docker/daemon.json
+    {
+    "exec-opts": ["native.cgroupdriver=systemd"],
+    "log-driver": "json-file",
+    "log-opts": {
+    "max-size": "100m"
+    },
+    "storage-driver": "overlay2"
+    }
